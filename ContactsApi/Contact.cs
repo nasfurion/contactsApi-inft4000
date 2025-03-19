585d319370b13e3f4e4c52c7ab95173356396cae
Nasfurion <diego.amadio93@gmail.com>
2025-03-19 16:17:12 -0300

    initial commit

---
+++ ContactsApi/Contact.cs
@@ -0,0 +1,8 @@
+﻿namespace ContactsApi
+{
+    public class Contact
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
---
+++ ContactsApi/Controllers/ContactsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ILogger<ContactsController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public ContactsController(ILogger<ContactsController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from Contacts Controller - GET()");
+        }
+
+        [HttpPost]
+        public IActionResult Post(Contact contact)
+        {
+            // Validation
+            if (string.IsNullOrEmpty(contact.FirstName) || string.IsNullOrEmpty(contact.LastName))
+            {
+                return BadRequest("First and Last name are required");
+            }
+            return Ok("Hello " + contact.FirstName + " " + contact.LastName + " from Contacts Controller - POST()");
+        }
+    }
+}
